111a85821289081cdbb5ac7ef1cc4e517ddfdade
VD Premathilaka <2021cs146@stu.ucsc.cmb.ac.lk>
2024-12-07 01:08:04 +0530

    added UIs for courier orders

--- app/controllers/Courier.php
+++ app/controllers/Courier.php
@@ -4,6 +4,15 @@ class Courier extends Controller
 {
     public function index()
     {
+        // show($_SESSION['USER_DATA']);
+        // die;
         $this->view('courier/dashboard');
+    }
+
+    public function profile() {}
+
+    public function completedOrders()
+    {
+        $this->view('courier/completedOrders');
     }
 }
---
+++ app/views/courier/completedOrders.view.php
@@ -0,0 +1,107 @@
+<?php $this->view('admin/includes/sidenav'); ?>
+<adminCourier>
+    <h1 class="title">Completed Orders</h1>
+
+    <table>
+        <tr>
+            <th>Order Id</th>
+            <th>Borrower Name</th>
+            <th>contact No.</th>
+            <th>Amount</th>
+            <th>Weight</th>
+            <th>Details</th>
+        </tr>
+        <tr>
+            <td>23</td>
+            <td>Vidath</td>
+            <td>772205749</td>
+            <td>Rs. 4500</td>
+            <td>675g</td>
+            <td><button>View Details</button></td>
+        </tr>
+        <tr>
+            <td>Vidath</td>
+            <td>8/2, Wata mawatha, Horathuduwa, Polgasowita</td>
+            <td>772205749</td>
+            <td><span class="material-symbols-outlined status" style="position: relative; top:0; left:25px">toggle_on</span>
+            </td>
+            <td><span class="material-symbols-outlined" style="position: relative; top:0; left:0">
+                    edit
+                </span>
+                <span class="material-symbols-outlined" style="position: relative; top:0; left:10px">
+                    delete
+                </span>
+            </td>
+        </tr>
+        <tr>
+            <td>Vidath</td>
+            <td>8/2, Wata mawatha, Horathuduwa, Polgasowita</td>
+            <td>772205749</td>
+            <td><span class="material-symbols-outlined status" style="position: relative; top:0; left:25px">toggle_on</span>
+            </td>
+            <td><span class="material-symbols-outlined" style="position: relative; top:0; left:0">
+                    edit
+                </span>
+                <span class="material-symbols-outlined" style="position: relative; top:0; left:10px">
+                    delete
+                </span>
+            </td>
+        </tr>
+        <tr>
+            <td>Vidath</td>
+            <td>8/2, Wata mawatha, Horathuduwa, Polgasowita</td>
+            <td>772205749</td>
+            <td><span class="material-symbols-outlined status" style="position: relative; top:0; left:25px">toggle_on</span>
+            </td>
+            <td><span class="material-symbols-outlined" style="position: relative; top:0; left:0">
+                    edit
+                </span>
+                <span class="material-symbols-outlined" style="position: relative; top:0; left:10px">
+                    delete
+                </span>
+            </td>
+        </tr>
+        <tr>
+            <td>Vidath</td>
+            <td>8/2, Wata mawatha, Horathuduwa, Polgasowita</td>
+            <td>772205749</td>
+            <td><span class="material-symbols-outlined status" style="position: relative; top:0; left:25px">toggle_on</span>
+            </td>
+            <td><span class="material-symbols-outlined" style="position: relative; top:0; left:0">
+                    edit
+                </span>
+                <span class="material-symbols-outlined" style="position: relative; top:0; left:10px">
+                    delete
+                </span>
+            </td>
+        </tr>
+        <tr>
+            <td>Vidath</td>
+            <td>8/2, Wata mawatha, Horathuduwa, Polgasowita</td>
+            <td>772205749</td>
+            <td><span class="material-symbols-outlined status" style="position: relative; top:0; left:25px">toggle_on</span>
+            </td>
+            <td><span class="material-symbols-outlined" style="position: relative; top:0; left:0">
+                    edit
+                </span>
+                <span class="material-symbols-outlined" style="position: relative; top:0; left:10px">
+                    delete
+                </span>
+            </td>
+        </tr>
+        <tr>
+            <td>Vidath</td>
+            <td>8/2, Wata mawatha, Horathuduwa, Polgasowita</td>
+            <td>772205749</td>
+            <td><span class="material-symbols-outlined status" style="position: relative; top:0; left:25px">toggle_on</span>
+            </td>
+            <td><span class="material-symbols-outlined" style="position: relative; top:0; left:0">
+                    edit
+                </span>
+                <span class="material-symbols-outlined" style="position: relative; top:0; left:10px">
+                    delete
+                </span>
+            </td>
+        </tr>
+    </table>
+</adminCourier>
